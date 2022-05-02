20356e1f2872f8c6e8dfeb96e214db1d1e13533d
CNshilpa <shilpa.cnv@gmail.com>
2022-05-02 19:02:26 +0530

    Addition: present/Absent

--- employee.sh
+++ employee.sh
@@ -1,1 +1,12 @@
-Welcome to Employee Wage Computation Program 
+echo "Welcome to Employee Wage Computation Program" 
+function myfunction()
+{
+echo $1;
+}
+result=$( myfunction $(( RANDOM%2 )))
+if(( $result ==0 ))
+then
+echo "Employee is absent"
+else
+echo "Employee is present"
+fi
